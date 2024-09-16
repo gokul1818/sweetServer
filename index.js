0360913bee91aa31104dcc64f5d49438dd452344
gokul18 <gokulakrishnan.doodleblue@gmail.com>
2024-09-16 12:20:37 +0530

    udpaye

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const app = express();
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+
 })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
