90bd11dbb775d2789dfc631aafcf9c3a71881dee
gokul18 <gokulakrishnan.doodleblue@gmail.com>
2024-09-16 12:22:00 +0530

    udpate

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const categoriesRoutes = require('./src/routes/categories');
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
