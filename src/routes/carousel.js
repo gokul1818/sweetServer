1256436fa8f02f6c65f3d4b40b67009d6a670cb7
gokul18 <gokulakrishnan.doodleblue@gmail.com>
2024-09-16 18:01:13 +0530

    udpate

--- src/routes/carousel.js
+++ src/routes/carousel.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const carouselController = require("../controllers/carouselController"); // Assuming productsController for consistency
 
-router.post("/carousel", carouselController.addNewProducts);
-router.get("/carousel", carouselController.getAllProductsList);
-router.delete("/carousel", carouselController.deleteProductById);
+router.post("/carousel", carouselController.addNewCarousel);
+router.get("/carousel", carouselController.getAllCarousel);
+router.delete("/carousel", carouselController.deleteCarouselById);
 
 module.exports = router;
     
