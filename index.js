2530112cbf6a9cb3c5b355328716c6de2cf3069c
gokul18 <gokulakrishnan.doodleblue@gmail.com>
2024-09-18 07:40:00 +0530

    udpatee

--- index.js
+++ index.js
@@ -13,13 +13,10 @@ const app = express();
 // Connect to MongoDB
 (async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
-    await importData();
-cronScheduler.start();
+    // await importData();
+// cronScheduler.start();
 
 
   } catch (err) {
