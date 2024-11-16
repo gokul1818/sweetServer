0bceb04e58e65f8bf92cdf1925e20bf3260bc26b
gokul18 <gokulakrishnan.doodleblue@gmail.com>
2024-11-16 20:38:22 +0530

    update

--- src/models/messages.js
+++ src/models/messages.js
@@ -4,6 +4,10 @@ const messageSchema = new mongoose.Schema({
   roomId: String,
   username: String,
   message: String,
+  image: {
+    type: Buffer, // Store image data as binary
+    required: false, // Make it optional in case not every message has an image
+  },
   timestamp: {
     type: Date,
     default: Date.now,
